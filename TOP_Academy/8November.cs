f55c52eef122fbd2323bb84a7be695fb8eed10f8
noanonofi@yandex.ru <noanonofi@yandex.ru>
2022-11-09 10:09:02 +0500

    Добавил классную работу из академии

--- TOP_Academy/8November.cs
+++ TOP_Academy/8November.cs
@@ -4,9 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TOP_Academy
+namespace Classwork_8_november
 {
-    internal class _8November
+    class StudyClassWork
     {
+        interface IPropertyChanged
+        {
+            event PropertyEventHandler PropertyChanged;
+        }
+
+        public class PropertyEventArgs : EventArgs
+        {
+            public string PropertyName { get; set; }
+        }
+
+        public delegate void PropertyEventHandler(object sender, PropertyEventArgs e);
+
+        public class PropertyChanged__ : IPropertyChanged
+        {
+            public event PropertyEventHandler PropertyChanged;
+
+            private string temp;
+            public string Temp
+            {
+                get { return temp; }
+                set
+                {
+                    if (temp != value)
+                    {
+                        temp = value;
+                        PropertyEventArgs propertyEventArgs = new PropertyEventArgs();
+                        propertyEventArgs.PropertyName = nameof(value);
+                        PropertyChanged?.Invoke(this, propertyEventArgs);
+                    }
+                }
+            }
+        }
     }
 }
--- TOP_Academy/Program.cs
+++ TOP_Academy/Program.cs
@@ -1,22 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using static Classwork_8_november.StudyClassWork;
 
-namespace TOP_Academy
+namespace Classwork_8_november
 {
-    internal static class Program
+    #region ClassHello
+    delegate void SendMessage(string message);
+    class Hello
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public void Display(string message)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine(message);
+        }
+
+        public void SpellItOut(string message)
+        {
+            foreach (var ch in message)
+            {
+                Console.Write($"{ch} ");
+            }
+        }
+        public void Reverse(string message)
+        {
+            var str = new String(message.Reverse().ToArray());
+            Console.WriteLine(str);
         }
     }
+    #endregion
+
+    #region BankAccount
+    delegate void BankAccountHandler(decimal sum);
+    class BankAccount
+    {
+        public BankAccountHandler BankAccountHandler;
+        public void Add(decimal sum)
+        {
+            BankAccountHandler?.Invoke(sum);
+        }
+    }
+    class Client
+    {
+        private readonly string _name;
+        private readonly BankAccount _bankAccount;
+        public Client(string name, BankAccount bankAccount)
+        {
+            _name = name;
+            _bankAccount = bankAccount;
+            bankAccount.BankAccountHandler = AccountSumChanged;
+        }
+        private void AccountSumChanged(decimal sum)
+        {
+            Console.WriteLine($"The client's account has changed. Sum: {sum}");
+        }
+    }
+    #endregion
+
+    #region EventArgs
+
+    public class FallsIllEventArgs : EventArgs
+    {
+        public string Address;
+    }
+
+    public class Person
+    {
+        public void CatchACold()
+        {
+            FallsIll?.Invoke(this,
+                new FallsIllEventArgs { Address = "123 London Road" });
+        }
+
+        public event EventHandler<FallsIllEventArgs> FallsIll;
+    }
+
+    #endregion
+
+    #region Main
+    class Program
+    {
+        // ---- #region EventArgs
+        private static void CallDoctor(object sender, FallsIllEventArgs eventArgs)
+        {
+            Console.WriteLine($"A doctor has been called to {eventArgs.Address}");
+        }
+
+        static void Main(string[] args)
+        {
+
+            /*
+             * --- #region ClassHello
+            var hello = new Hello();
+            SendMessage sendMessage = hello.Display;
+
+            sendMessage += hello.SpellItOut;
+            sendMessage += hello.Reverse;
+            sendMessage("Привет");
+            Console.WriteLine();
+
+            //Удалим из цепочки один из методов
+            sendMessage -= hello.SpellItOut;
+            sendMessage -= hello.Reverse;
+            sendMessage("До свидания");
+             */
+
+            /* ---- #region BankAccount    
+            var bankAccount = new BankAccount();
+            var client = new Client("Alex", bankAccount);
+            bankAccount.Add(45);
+
+            var hello = new Hello();
+            var action = new Action<string>(hello.Display);
+            action += hello.SpellItOut;
+            action("Hello");
+            */
+
+            // ---- #region EventArgs
+            /*            var person = new Person();
+
+                        person.FallsIll += CallDoctor;
+
+                        person.CatchACold();*/
+
+            var propperty = new PropertyChanged__();
+            propperty.PropertyChanged += IsShanges;
+            propperty.Temp = "KEK";
+
+        }
+        private static void IsShanges(object sender, PropertyEventArgs eventArgs)
+        {
+            Console.WriteLine($"Word {eventArgs.PropertyName} changes in {sender}");
+        }
+    }
+
+    #endregion
 }
